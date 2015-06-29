1050bb2eea35349b4f9babbcc866b9d1d64d5f07
jonathan <jonathan@jonathananderson.se>
2015-06-29 16:51:50 +0200

    organisation model

--- iportalen/organisations/models.py
+++ iportalen/organisations/models.py
@@ -1,3 +1,11 @@
+from iportalen import settings
 from django.db import models
 
 # Create your models here.
+class Organisation(models.Model):
+    name = models.CharField(max_length=255, unique=True, null=False)
+    description = models.TextField()
+    contact_info = models.TextField()
+    image = models.FileField(upload_to='organisations')
+    leader = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, related_name="leader")
+    members = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="members")
