03b318d475085ccde5378f5af18e357d1d7a7238
Isac Ekberg <isaek808@student.liu.se>
2015-07-02 22:08:04 +0200

    Added requirement to setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name='I-Portalen',
       author='Webgroup',
       author_email='webmaster@isektionen.se',
       url='http://www.i-portalen.se',
-      install_requires=['Django>=1.8', 'PyMySQL == 0.6.6', 'dj-static'],  #This should match requirements.txt!
+      install_requires=['Django>=1.8', 'PyMySQL == 0.6.6', 'dj-static', 'django-reversion'],  #This should match requirements.txt!
       )
