7def22aa5042263d1400e83624f071cf958873e7
Isac Ekberg <isaek808@student.liu.se>
2015-07-13 22:18:47 +0200

    WSGI-callable application working
    The file wsgi/application.py is the callable that openshift uses to start
    django. It is now working. Important is to import the right database backend.

--- wsgi/iportalen_django/manage.py
+++ wsgi/iportalen_django/manage.py
@@ -3,6 +3,15 @@ import os
 import sys
 
 if __name__ == "__main__":
+    """
+    This part here imports pymysql and uses it instead of MySQLdb which is not proted to python3 yet.
+
+    It might cause problems with advanced mysql-operations.
+
+    The same trick is done in the application.py.
+
+    //Isac
+    """
     try:
         import pymysql
         pymysql.install_as_MySQLdb()
