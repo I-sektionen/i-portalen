bc02e61c44a1cbeda051a0b6edb3291123c562ac
Andreas G <andgu034@student.liu.se>
2015-07-15 19:59:51 +0200

    added single view for articles on read more btn

--- wsgi/iportalen_django/articles/urls.py
+++ wsgi/iportalen_django/articles/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<article_id>[0-9]+)/$', views.single_article, name='article')
 
 ]
--- wsgi/iportalen_django/articles/views.py
+++ wsgi/iportalen_django/articles/views.py
@@ -7,3 +7,7 @@ def index(request):
     print(articles)
     context = {'articles': articles}
     return render(request, 'articles/index.html', context)
+
+def single_article(request, article_id):
+    article = Article.objects.get(pk = article_id)
+    return render(request, 'articles/article.html', {'article': article})
