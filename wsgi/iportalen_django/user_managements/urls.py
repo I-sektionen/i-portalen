36fe1aba236cd8dbadd6733770ffac37428099c8
jonathan <jonathan@jonathananderson.se>
2015-09-07 16:08:58 +0200

    Cooler login than Isacs....

--- wsgi/iportalen_django/user_managements/urls.py
+++ wsgi/iportalen_django/user_managements/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url, patterns
 
-from .views import logout_view
+from .views import logout_view, login
 
 urlpatterns = patterns('',
     url(r'^logout$', logout_view, name='logout_view'),
-    url(r'login', 'django.contrib.auth.views.login', {'template_name': 'user_managments/login.html'}, name='login_view')
+    # url(r'login', 'django.contrib.auth.views.login', {'template_name': 'user_managments/login.html'}, name='login_view')
+    url(r'^login$', login, name='login_view'),
    )
--- wsgi/iportalen_django/user_managements/views.py
+++ wsgi/iportalen_django/user_managements/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import redirect
-from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login, logout
 
 
 def logout_view(request):
     logout(request)
     return redirect('/')  # TODO: Where should this redirect?
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['liu-id']
+        password = request.POST['password']
+        user = authenticate(username=username.lower(), password=password)
+        if user is not None:
+            # the password verified for the user
+            if user.is_active:
+                auth_login(request, user)
+                try:
+                    return redirect(request.POST['from_url'])
+                except:
+                    return redirect('/')
+            else:
+                return render(request, "user_managements/login.html",
+                          {'message': "The password is valid, but the account has been disabled!"})
+                # The password is valid, but the account has been disabled!
+        else:
+            # the authentication system was unable to verify the username and password
+            return render(request, "user_managements/login.html",
+                          {'message': "The username and password were incorrect."})
+    else:
+        return render(request, "user_managements/login.html")
