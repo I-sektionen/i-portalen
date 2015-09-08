cab3b29740dea6859a8a1a8fbd77b5361d055b8c
Jesper Lehtonen <lehtonen@c-226-78.eduroam.liu.se>
2015-09-08 10:39:19 +0200

    Test commit in javascript-menu branch

--- wsgi/static/js/iportalen.js
+++ wsgi/static/js/iportalen.js
@@ -4,13 +4,44 @@ $(document).ready(function () {
     var menuToggle = $('#menu-toggle').unbind();
     var navigationMenu = $('#navigation-menu').removeClass('show');
     var submenuWrapper = $('.submenu-wrapper').removeClass('show');
-    menuToggle.on('click', function (event) {
+
+    $('#navigation-menu > li.more a').on('mouseover', openSubMenu);
+    $('#navigation-menu > li.more a').on('mouseout', openSubMenu);
+
+    menuToggle.on('mouseover', openSubMenu);
+    menuToggle.on('mouseout', openSubMenu);
+
+    function openSubMenu() {
+        //$(this).next().toggleClass('show');
+        $(this).find('ul').toggleClass('show');
+    }
+
+/*    menuToggle.on('mouseover', function (event) {
         event.preventDefault();
         navigationMenu.toggleClass('show');
     });
 
-    $('li.more a').on('click', function (event) {
+    $('li.more a').on('mouseover', function (event) {
         event.preventDefault();
         $(this).next().toggleClass('show');
     });
+    */
+
+/*    menuToggle.hover(
+        function (event) {
+        event.preventDefault();
+        navigationMenu.toggleClass('show');
+    }, function (event) {
+        event.preventDefault();
+        navigationMenu.toggleClass('show');
+    });
+
+    $('li.more a').hover(function (event) {
+        event.preventDefault();
+        $(this).next().toggleClass('show');
+    }, function (event) {
+        event.preventDefault();
+        $(this).next().toggleClass('show');
+    });
+*/
 });
