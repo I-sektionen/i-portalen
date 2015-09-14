f8e9e608be4a727880d73960d9e57fe243224115
Jesper Lehtonen <lehtonen@c-227-74.eduroam.liu.se>
2015-09-14 19:56:08 +0200

    Fixed dropdown menus, should be working on both mobile devices and computer

--- wsgi/static/js/iportalen.js
+++ wsgi/static/js/iportalen.js
@@ -5,20 +5,48 @@ $(document).ready(function () {
     var navigationMenu = $('#navigation-menu').removeClass('show');
     var submenuWrapper = $('.submenu-wrapper').removeClass('show');
 
-    menuToggle.on('mouseover', openSubMenu);
-    $('#navigation-menu > li').on('mouseover', openSubMenu);
-    $(".nav-bar").on('mouseover', closeSubMenu);
-    $('.hero').on('mouseover', closeSubMenu);
+    menuToggle.on('click', function (event) {
+        event.preventDefault();
+        navigationMenu.toggleClass('show');
+        console.log("menuToggle click");
+    });
+
+    $('#navigation-menu > li').on('touchstart click', function (event) {
+        event.stopPropagation();
+        event.preventDefault();
+        // Prevents the doubleclick while on touchdevices
+        if(event.handled !== true) {
+            if ($(".submenu-wrapper").hasClass("show")) {
+                closeSubMenu();
+            } else {
+                // I can't use the openSubMenu since "this" dosen't follow with it -
+                // might work on a solution on this later
+                //openSubMenu();
+
+                $(".submenu-wrapper").removeClass("show");
+                $(this).children(".submenu-wrapper").addClass("show");
+            }
+            event.handled = true;
+        } else {
+            return false;
+        }
+    });
+
+    // Works the hover aspect while not on a mobile device
+    $('#navigation-menu > li ').hover(openSubMenu);
+
 
     function openSubMenu() {
-        $(".submenu-wrapper").removeClass("show");
-        //closeSubMenu();
+        closeSubMenu();
         $(this).children(".submenu-wrapper").addClass('show');
-        //$(this).find('ul').toggleClass('show');
     };
 
     function closeSubMenu() {
         $(".submenu-wrapper").removeClass("show");
     };
+
+    // Closes the hover aspect of the menu when mouse gets on another part of the page
+    $(".nav-bar").on('mouseover', closeSubMenu);
+    $('.hero').on('mouseover', closeSubMenu);
 
 });
