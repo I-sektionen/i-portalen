4d35a31696a482e9df4d8d940faca05c74cd457e
jonathan <jonathan@jonathananderson.se>
2015-10-11 16:43:26 +0200

    organisation styling and fixed is_debug template tag.

--- wsgi/iportalen_django/iportalen/templatetags/iportalen_tags.py
+++ wsgi/iportalen_django/iportalen/templatetags/iportalen_tags.py
@@ -1,9 +1,10 @@
+from django import template
 from django.template.loader_tags import register
 from django.conf import settings
-
+register = template.Library()
 
 @register.assignment_tag
-def in_debug():
+def is_debug():
     return settings.DEBUG
 
 
