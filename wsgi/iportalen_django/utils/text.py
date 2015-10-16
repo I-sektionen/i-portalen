844871e442e3e762c777c0612ed1c8be8f338bab
jonathan <jonan099@student.liu.se>
2015-10-16 16:32:13 +0200

    max 160 characters in lead

--- wsgi/iportalen_django/utils/text.py
+++ wsgi/iportalen_django/utils/text.py
@@ -3,3 +3,6 @@ __author__ = 'jonathan'
 
 def count_words(text):
     return len(text.split())
+
+def count_characters(text):
+    return len(text)
--- wsgi/iportalen_django/utils/validators.py
+++ wsgi/iportalen_django/utils/validators.py
@@ -1,11 +1,15 @@
 __author__ = 'jonathan'
 from django.core.validators import RegexValidator
 from django.core.exceptions import ValidationError
-from .text import count_words
+from .text import count_words, count_characters
 
 liu_id_validator = RegexValidator(r'^[a-zA-Z]{5}\d{3}$')
 slash_validator = RegexValidator(r'^[^/]+$', "Kan inte innehålla '/'")
 
 def less_than_200_words_validator(value):
     if count_words(value) > 201:
-        raise ValidationError('För många ord')
+        raise ValidationError('För många ord.')
+
+def less_than_160_characters_validator(value):
+    if count_characters(value) > 160:
+        raise ValidationError('För många tecken.')
