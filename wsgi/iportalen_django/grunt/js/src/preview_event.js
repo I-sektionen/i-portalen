9af6f4588ca1a3abb7e51d046635c87b30c88fbd
Isac Ekberg <isaek808@student.liu.se>
2015-10-19 20:38:40 +0200

    Article preview, better.

--- wsgi/iportalen_django/grunt/js/src/preview_event.js
+++ wsgi/iportalen_django/grunt/js/src/preview_event.js
@@ -96,75 +96,7 @@ var event_preview = function () {
         ele.change(function(){render()});
         ele.keypress(function(){render()});
     });
-
-    /**
-    var txt=headline.val();
-    $("#headline_collapsed").text(txt);
-    $("#headline_expanded").text(txt);
-    headline.keyup(function(event) {
-        var txt=$(this).val();
-        $("#headline_collapsed").text(txt);
-        $("#headline_expanded").text(txt);
-    });
-    txt=headline.val();
-    lead_collapsed.text(txt);
-    $("#lead_expanded").text(txt);
-    var wordCounts = {};
-    lead.keyup(function(event) {
-        var number = 0;
-        var matches = $(this).val().match(/\b/g);
-        if (matches) {
-            number = matches.length / 2;
-        }
-        wordCounts[this] = number;
-        var finalCount = 0;
-        $.each(wordCounts, function(k, v) {
-            finalCount += v;
-        });
-        var txt=$(this).val();
-        if (finalCount<=50){
-            lead_collapsed.text(txt);
-        }
-        $("#lead_expanded").text(txt);
-    });
-
-    var fullDate = new Date();
-    var twoDigitMonth = fullDate.getMonth()+"";
-    if(twoDigitMonth.length==1){
-        twoDigitMonth="0" +twoDigitMonth;
-    }
-    var twoDigitDate = fullDate.getDate()+"";
-    if(twoDigitDate.length==1){
-        twoDigitDate="0" +twoDigitDate;
-    }
-    var currentDate = fullDate.getFullYear() + "-" + twoDigitMonth + "-" + twoDigitDate;
-    $("#date_collapsed").text(currentDate);
-    $("#date_expanded").text(currentDate);
-
-    var author = $("#id_author");
-    txt=author.val();
-    $("#author_collapsed").text(txt);
-    $("#author_expanded").text(txt);
-
-    author.keyup(function(event) {
-        var txt=$(this).val();
-        $("#author_collapsed").text(txt);
-        $("#author_expanded").text(txt);
-    });
-     */
 };
 
 
 
-/**
-function displayVals() {
-    var multipleValues = $("#id_tags option:selected").map(function() {
-        return $(this).text();
-    }).get();
-    $( "#tags_collapsed" ).html( multipleValues.join( ", " ) );
-    $( "#tags_expanded" ).html( multipleValues.join( ", " ) );
-}
-$( "select" ).change( displayVals );
-displayVals();
-
-**/
--- wsgi/iportalen_django/local_static/js/iportalen.js
+++ wsgi/iportalen_django/local_static/js/iportalen.js
@@ -4070,16 +4070,9 @@ $(document).ready(function () {
     })**/
 
 });
-;/**
- * Created by jonathan on 2015-09-08.
- */
-/*
-(function () {
-        //var converter1 = Markdown.getConverter();
-        var converter1 = Markdown.getSanitizingConverter();
-
-
-        converter1.hooks.chain("preConversion", function (text) {
+;function markdown_event_preview() {
+        var converter = Markdown.getSanitizingConverter();
+        converter.hooks.chain("preConversion", function (text) {
             return text.replace(/[&<"'\/]/g, function (s) {
                 var entityMap = {
                     "&": "&amp;",
@@ -4091,83 +4084,82 @@ $(document).ready(function () {
                 return entityMap[s];
             }).replace(/([#]{2,})/g, '#').replace(/([=]{3,})/g, '').replace(/([-]{3,})/g, '').replace(/([`])/g, '');
         });
-
-        converter1.hooks.chain("plainLinkText", function (url) {
+        converter.hooks.chain("plainLinkText", function (url) {
             return url.replace(/^https?:\/\//, "");
         });
-        var editor1 = new Markdown.Editor(converter1, "-body");
-        editor1.run();
+        var editor = new Markdown.Editor(converter, "-body");
+        editor.run();
+    }
+/**
+ * This function
+ * 1. Starts the markdown engine.
+ * 2. Add event listeners on the forms.
+ */
+var article_preview = function () {
+    //Start the engines. Listens to the body and outputs.
+    console.log("Call on meeeee");
+    markdown_event_preview();
+    console.log("Call on me");
 
-    })();
-
+    //These are the elements that is being listened to.
+    var listened_to = [];
     var headline = $("#id_headline");
-    var txt=headline.val();
-    $("#headline_collapsed").text(txt);
-    $("#headline_expanded").text(txt);
-
-    headline.keyup(function(event) {
-        var txt=$(this).val();
-        $("#headline_collapsed").text(txt);
-        $("#headline_expanded").text(txt);
-    });
-
     var lead = $("#id_lead");
-    var lead_collapsed = $("#lead_collapsed");
-    txt=headline.val();
-    lead_collapsed.text(txt);
-    $("#lead_expanded").text(txt);
-    var wordCounts = {};
-    lead.keyup(function(event) {
-        var number = 0;
-        var matches = $(this).val().match(/\b/g);
-        if (matches) {
-            number = matches.length / 2;
-        }
-        wordCounts[this] = number;
-        var finalCount = 0;
-        $.each(wordCounts, function(k, v) {
-            finalCount += v;
-        });
-        var txt=$(this).val();
-        if (finalCount<=50){
-            lead_collapsed.text(txt);
-        }
-        $("#lead_expanded").text(txt);
-    });
-
-    var fullDate = new Date();
-    var twoDigitMonth = fullDate.getMonth()+"";
-    if(twoDigitMonth.length==1){
-        twoDigitMonth="0" +twoDigitMonth;
-    }
-    var twoDigitDate = fullDate.getDate()+"";
-    if(twoDigitDate.length==1){
-        twoDigitDate="0" +twoDigitDate;
-    }
-    var currentDate = fullDate.getFullYear() + "-" + twoDigitMonth + "-" + twoDigitDate;
-    $("#date_collapsed").text(currentDate);
-    $("#date_expanded").text(currentDate);
-
     var author = $("#id_author");
-    txt=author.val();
-    $("#author_collapsed").text(txt);
-    $("#author_expanded").text(txt);
+    var from = $("#id_visible_from");
+    var tags = $("#id_tags");
 
-    author.keyup(function(event) {
-        var txt=$(this).val();
-        $("#author_collapsed").text(txt);
-        $("#author_expanded").text(txt);
+    //This is the elements where the results are printed to.
+    var headline_preview = $(".headline_preview");
+    var lead_preview = $(".lead_preview");
+    var author_preview = $(".author_preview");
+    var from_preview_date = $(".date_preview");
+    var tags_preview = $(".tags_preview");
+
+    listened_to.push(
+                    [headline, headline_preview],
+                    [lead, lead_preview],
+                    [author, author_preview],
+                    [from, from_preview_date],
+                    [tags, tags_preview]
+    );
+
+    var render = function (){
+        console.log("Bamm.");
+        jQuery.each(listened_to, function(index, element){
+            var txt;
+
+            //Special case of start-time:
+            if(element[0].is(from)) {
+                if (element[0].val() != "") {
+                    var d = element[0].val().split(" ");
+                    var date = d[0];
+                    element[1].text(date);
+                }
+            } else if(element[0].is(tags)){
+                var selected_tags = tags.children().filter(":selected");
+                var tag_string = "";
+                jQuery.each(selected_tags, function(index, ele){
+                    tag_string = tag_string + ele.innerHTML + ", ";
+                });
+                if(tag_string != ""){
+                    tag_string = tag_string.substring(0, tag_string.length-2)
+                }
+                element[1].text(tag_string);
+
+            } else {
+                txt = element[0].val();
+                element[1].text(txt);
+            }
+        });
+    };
+
+    jQuery.each(listened_to, function(index, element){
+        var ele = element[0];
+        ele.change(function(){render()});
+        ele.keypress(function(){render()});
     });
-function displayVals() {
-    var multipleValues = $("#id_tags option:selected").map(function() {
-        return $(this).text();
-    }).get();
-    $( "#tags_collapsed" ).html( multipleValues.join( ", " ) );
-    $( "#tags_expanded" ).html( multipleValues.join( ", " ) );
-}
-$( "select" ).change( displayVals );
-displayVals();
-*/;//This function initiates the markdown engine. It is called on by event_preview below.
+};;//This function initiates the markdown engine. It is called on by event_preview below.
 function markdown_event_preview() {
         var converter = Markdown.getSanitizingConverter();
         converter.hooks.chain("preConversion", function (text) {
@@ -4265,75 +4257,7 @@ var event_preview = function () {
         ele.change(function(){render()});
         ele.keypress(function(){render()});
     });
-
-    /**
-    var txt=headline.val();
-    $("#headline_collapsed").text(txt);
-    $("#headline_expanded").text(txt);
-    headline.keyup(function(event) {
-        var txt=$(this).val();
-        $("#headline_collapsed").text(txt);
-        $("#headline_expanded").text(txt);
-    });
-    txt=headline.val();
-    lead_collapsed.text(txt);
-    $("#lead_expanded").text(txt);
-    var wordCounts = {};
-    lead.keyup(function(event) {
-        var number = 0;
-        var matches = $(this).val().match(/\b/g);
-        if (matches) {
-            number = matches.length / 2;
-        }
-        wordCounts[this] = number;
-        var finalCount = 0;
-        $.each(wordCounts, function(k, v) {
-            finalCount += v;
-        });
-        var txt=$(this).val();
-        if (finalCount<=50){
-            lead_collapsed.text(txt);
-        }
-        $("#lead_expanded").text(txt);
-    });
-
-    var fullDate = new Date();
-    var twoDigitMonth = fullDate.getMonth()+"";
-    if(twoDigitMonth.length==1){
-        twoDigitMonth="0" +twoDigitMonth;
-    }
-    var twoDigitDate = fullDate.getDate()+"";
-    if(twoDigitDate.length==1){
-        twoDigitDate="0" +twoDigitDate;
-    }
-    var currentDate = fullDate.getFullYear() + "-" + twoDigitMonth + "-" + twoDigitDate;
-    $("#date_collapsed").text(currentDate);
-    $("#date_expanded").text(currentDate);
-
-    var author = $("#id_author");
-    txt=author.val();
-    $("#author_collapsed").text(txt);
-    $("#author_expanded").text(txt);
-
-    author.keyup(function(event) {
-        var txt=$(this).val();
-        $("#author_collapsed").text(txt);
-        $("#author_expanded").text(txt);
-    });
-     */
 };
 
 
 
-/**
-function displayVals() {
-    var multipleValues = $("#id_tags option:selected").map(function() {
-        return $(this).text();
-    }).get();
-    $( "#tags_collapsed" ).html( multipleValues.join( ", " ) );
-    $( "#tags_expanded" ).html( multipleValues.join( ", " ) );
-}
-$( "select" ).change( displayVals );
-displayVals();
-
-**/
