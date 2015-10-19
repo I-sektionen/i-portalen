5fdfb9096cf2074f5e089efd694197a9ab63c806
jonathan <jonan099@student.liu.se>
2015-10-19 15:09:34 +0200

    FFS

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name='I-Portalen',
       author='Webgroup',
       author_email='webmaster@isektionen.se',
       url='http://www.i-portalen.se',
-      install_requires=['Django>=1.8,<1.8.99', 'PyMySQL', 'dj-static', 'django-reversion', 'markdown', 'django-storages-redux', 'boto', 'requests'],  #This should match requirements.txt!
+      install_requires=['Django>=1.8,<1.8.99', 'PyMySQL', 'dj-static', 'django-reversion', 'markdown', 'django-storages-redux', 'boto', 'urllib3'],  #This should match requirements.txt!
       )
