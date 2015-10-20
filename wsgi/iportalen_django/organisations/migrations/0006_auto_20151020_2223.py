62eb6f0859b58e02f06bdf001da4e1b1571db65d
Jesper Lehtonen <jesle149@student.liu.se>
2015-10-20 23:48:11 +0200

    Possible to attach file to article, and possible to change image in organisation

---
+++ wsgi/iportalen_django/organisations/migrations/0006_auto_20151020_2223.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('organisations', '0005_auto_20151012_1943'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='organisation',
+            name='description',
+            field=models.TextField(null=True, verbose_name='Beskrivning', help_text='Beskrivning av organisationen', blank=True),
+        ),
+    ]
