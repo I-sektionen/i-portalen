21164fcb7a22e9a77482c5db13f8a40f1a6badfb
jonathan <jonan099@student.liu.se>
2015-10-20 22:16:17 +0200

    sponsors

--- wsgi/iportalen_django/local_static/js/iportalen.js
+++ wsgi/iportalen_django/local_static/js/iportalen.js
@@ -4261,3 +4261,13 @@ var event_preview = function () {
 
 
 
+;/**
+ * Created by jonathan on 2015-10-20.
+ */
+    var shuffle_sponsors = function () {
+        var parent = $("#partners");
+        var divs = parent.children();
+        while (divs.length) {
+            parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
+        }
+    };
