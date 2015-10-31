e5791888dc2a8d500d6dfceefb96891acdf9e817
mangy92 <magfo425@student.liu.se>
2015-10-31 14:34:35 +0100

    Created a sliding panel with javascript

--- wsgi/iportalen_django/local_static/js/iportalen.js
+++ wsgi/iportalen_django/local_static/js/iportalen.js
@@ -4271,3 +4271,13 @@ var event_preview = function () {
             parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
         }
     };
+;/**
+ * Created by MagnusForzelius on 2015-10-31.
+ */
+
+var sliding_panel = function (){
+    $('.sliding-panel-button,.sliding-panel-fade-screen,.sliding-panel-close').on('click touchstart',function (e) {
+        $('.sliding-panel-content,.sliding-panel-fade-screen').toggleClass('is-visible');
+        e.preventDefault();
+    });
+};
