a68f08c60ea11989d786e403bb1a311c0cec3ce0
jonathan <jonan099@student.liu.se>
2015-11-01 21:50:13 +0100

    bugfixing... red text on errors in forms see events that you have registered on. removed name from form fields for change user info changed article permissions so that you are allowed to change it if you are the creator.

--- wsgi/iportalen_django/user_managements/forms.py
+++ wsgi/iportalen_django/user_managements/forms.py
@@ -61,7 +61,7 @@ class CustomUserChangeForm(UserChangeForm):
 class ChangeUserInfoForm(forms.ModelForm):
     class Meta:
         model = IUser
-        fields = ('first_name', 'last_name', 'address', 'zip_code', 'city', 'gender', 'allergies', 'start_year', 'expected_exam_year')
+        fields = ('address', 'zip_code', 'city', 'gender', 'allergies', 'start_year', 'expected_exam_year')
 
 class AddWhiteListForm(forms.Form):
     users = forms.CharField(
