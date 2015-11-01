9adadd0626250a6c1ef8733e019637a63d85bfe3
mangy92 <magfo425@student.liu.se>
2015-11-01 20:05:50 +0100

    Base for event testing

--- wsgi/iportalen_django/events/tests.py
+++ wsgi/iportalen_django/events/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.contrib.auth.models import Group, Permission
 
-# Create your tests here.
+from user_managements.models import IUser
+from .models import Event
+
+class EventTests(TestCase):
+    def setUp(self):
+        author = IUser.objects.create_user(username="testa123")
+        member = IUser.objects.create_user(username="testa321")
+        not_member = IUser.objects.create_user(username="liuid123")
+        info = IUser.objects.create_user(username="liuid666")
+        info.save()
+
+        e = Event.objects.create(name="Eventnamn")
+        e.headline = "Eventheader"
+        e.lead = "Eventlead"
+        e.body = "Eventbody"
+        e.location = "C1"
+
+        e.start = "2015-11-04 15:34"
+        e.end = "2015-11-04 16:56"
+
+        e.enable_registration = "true"
+        e.registration_limit = 2
+
+
+        e.save()
+
+    #def test_create_event(self):
+
+    #def test_registration_limit(self):
+
