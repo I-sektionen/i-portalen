561336f5cd32fd57fcd8a5d0fdfaae9c3d1f85a0
jonathan <jonathan@jonathananderson.se>
2015-11-08 20:20:11 +0100

    Added conversion script between island-id and liu-id. Added import registrations to event

---
+++ wsgi/iportalen_django/iportalen/tools/convert_island_to_liu.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+import sys
+import argparse
+import re
+import codecs
+
+def main(argv):
+    parser = argparse.ArgumentParser(description='Translate islands-id')
+    parser.add_argument("-i", "--input", dest="input",
+                        help="File with list of islands-id to translate", metavar="ISLAND")
+
+    parser.add_argument("-o", "--output", dest="output",
+                        help="File with list of Liu-id after translation", metavar="LIU")
+
+    parser.add_argument("-is", "--input_separator", dest="input_separator",
+                        help="Separator of file with list of islands-id", metavar="SEPARATOR")
+
+    parser.add_argument("-ic", "--input_column", dest="input_column",
+                        help="Column for islands-id in input file", metavar="COLUMN", type=int)
+    parser.add_argument("-ie", "--input_encoding", dest="input_encoding",
+                        help="Encoding for input file", metavar="ENCODING", default="utf-8")
+
+    parser.add_argument("-t", "--translator", dest="translator",
+                        help="File with list of translation between islands-id and Liu-id after translation",
+                        metavar="TRANSLATOR")
+
+    parser.add_argument("-q", "--quiet",
+                        action="store_false", dest="verbose",
+                        help="Don't print status messages to stdout")
+
+    args = parser.parse_args()
+    quiet = not args.verbose
+    if not quiet:
+        print("input:", args.input)
+        print("output:", args.output)
+        print("translator:", args.translator)
+    input_file = codecs.open(args.input, 'r', args.input_encoding)
+    output = open(args.output, 'w')
+    translator = open(args.translator, 'r')
+    trans_dict = {}
+    pattern = re.compile("^([A-Za-z0-9=]+)$")
+
+    for line in translator:
+        line = line.replace("\n", "")
+        if pattern.match(line):
+            line = line.replace("\n", "")
+            tmp = line.split('=')
+            trans_dict[tmp[0]] = tmp[1]
+        else:
+            if not quiet:
+                print("Error when reading translator line:", line)
+    translator.close()
+    islands_idn = []
+    for line in input_file:
+        try:
+            if args.input_separator:
+                islands_idn.append(line.split(args.input_separator)[args.input_column-1].replace('"',''))
+            else:
+                islands_idn.append(line)
+        except:
+            if not quiet:
+                print("Error when parsing line:", line)
+    input_file.close()
+    liu_id_list = []
+    for il in islands_idn:
+        try:
+            liu_id_list.append(trans_dict[il])
+        except:
+            if not quiet:
+                print("Error when translating following islands-id:", il)
+
+    for li in liu_id_list:
+        output.write(li+'\n')
+
+    output.close()
+if __name__ == "__main__":
+    main(sys.argv[1:])
