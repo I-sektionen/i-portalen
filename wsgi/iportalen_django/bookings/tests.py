a09487128dd69ea1a5a3f6e5af07700771d79e42
Isac Ekberg <isaek808@student.liu.se>
2015-11-18 23:00:39 +0100

    Make a booking

--- wsgi/iportalen_django/bookings/tests.py
+++ wsgi/iportalen_django/bookings/tests.py
@@ -1,22 +1,44 @@
 from django.test import TestCase
 from user_managements.models import IUser
+from datetime import date, time
 
-#from .models import Booking, BookingSlot, Bookable
+from .models import Booking, BookingSlot, Bookable, PartialBooking
 
-# Create your tests here.
-"""
+
 class EventTests(TestCase):
     def setUp(self):
         booker = IUser.objects.create_user(username="testa123")
         random_guy = IUser.objects.create_user(username="testa321")
         creator_pall = IUser.objects.create_user(username="liuid123")
+        booker.full_clean()
 
         bookable = Bookable.objects.create(name="bil", max_number_of_bookings=2)
-        BookingSlot.objects.create(start_time="08:00", end_time="12:00", bookable=bookable)
-        BookingSlot.objects.create(start_time="12:01", end_time="13:00", bookable=bookable)
-        BookingSlot.objects.create(start_time="13:01", end_time="16:00", bookable=bookable)
+        bookable.full_clean()
+
+        b1 = BookingSlot(start_time=time(hour=8, minute=0), end_time=time(hour=12, minute=0), bookable=bookable)
+        b1.full_clean()
+        b1.save()
+
+        BookingSlot.objects.create(start_time=time(hour=12, minute=1), end_time=time(hour=13, minute=0), bookable=bookable)
+        BookingSlot.objects.create(start_time=time(hour=13, minute=1), end_time=time(hour=17, minute=0), bookable=bookable)
 
     def test_make_a_booking(self):
+        booker = IUser.objects.get(username="testa123")
+        bookable = Bookable.objects.get(name="bil")
+        slots = BookingSlot.objects.filter(bookable=bookable).order_by("-start_time")
 
-        self.assertFalse(True)
-"""
+        print(slots[0])
+        print(slots[1])
+        print("ssdfdfs")
+
+
+        booking = Booking.objects.make_a_booking(bookable=bookable,
+                                                 start_date=date(year=2012, month=10, day=1),
+                                                 end_date=date(year=2012, month=10, day=2),
+                                                 start_slot=slots[0],
+                                                 end_slot=slots[1],
+                                                 user=booker)
+        self.assertIsNotNone(booking)
+        pbookings = PartialBooking.objects.filter(booking=booking)
+        print(pbookings)
+        self.assertTrue(pbookings.exists())
