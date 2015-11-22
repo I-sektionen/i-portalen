c6d052de995327c402cd4f94cc6ba909a3b9b543
Isac Ekberg <isaek808@student.liu.se>
2015-11-23 00:59:47 +0100

    jQuery like a boss

--- wsgi/iportalen_django/bookings/urls.py
+++ wsgi/iportalen_django/bookings/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     url(r'^book/(?P<bookable_id>[0-9]+)/$', views.make_booking, name="make_booking"),
     url(r'^book/(?P<bookable_id>[0-9]+)/(?P<year>[0-9]+)/(?P<week>[0-9]+)/$', views.make_booking, name="make_booking"),
+    url(r'book/(?P<bookable_id>[0-9]+)/api/$', views.api_view, name="api booking"),
+    url(r'book/(?P<bookable_id>[0-9]+)/api/(?P<weeks_forward>[0-9]+)/$', views.api_view, name="api booking week_forward"),
     url(r'^invoice/(?P<invoice_id>[0-9]+)', views.invoice_pdf, name="invoice_pdf"),
     url(r'^$', views.index, name="index"),
 ]
--- wsgi/iportalen_django/local_static/js/iportalen.js
+++ wsgi/iportalen_django/local_static/js/iportalen.js
@@ -4034,6 +4034,41 @@ else
     }
 })();
 ;/**
+ * Created by isac on 2015-11-22.
+ */
+function generate_booking_form(pk){
+    jQuery.get('booking/book/'+pk+"/api/" , function( data ){
+        var $element = jQuery(".booking");
+        var bookable = data.bookable;
+        var bookings = data.bookings;
+
+        $element.append("<div><p>" + bookable.name +"</p></div>");
+        var $days = $element.append("<div class=\"days\"></div>");
+
+        //Each day
+        jQuery.each( bookings, function(index, value){
+            var s = "<div>";
+            var d = value.date;
+            s = s + "<p>Datum: " + d.year + "-" + d.month + "-" + d.day;
+            //Each slot:
+            jQuery.each( value.slots, function( index, value2 ){
+                s = s + "<div class=\"slot";
+                if(value2.available){
+                    s = s + " available\">";
+                } else {
+                    s = s + " unavailable\">";
+                }
+                s = s + "<p>" + value2.start_time + "</p>";
+                s = s + "<p>" + value2.end_time + "</p>";
+                s = s + "</div>";
+            });
+             // Write values to child!
+            console.log(s);
+            $days.append(s);
+        });
+        console.log("hit.");
+    });
+};/**
  * Created by isac on 2015-10-08.
  */
 function closeMessage(element){
