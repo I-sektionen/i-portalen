4019d9d2879e5e7b325203f15b6478b17405d620
jonathan <jonan099@student.liu.se>
2015-11-23 22:22:19 +0100

    Probably the best bookings in town 2...

---
+++ wsgi/iportalen_django/bookings/migrations/0006_auto_20151123_1827.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('bookings', '0005_auto_20151121_0131'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='invoice',
+            name='due',
+            field=models.DateField(verbose_name='förfallo dag', default=datetime.datetime(2015, 12, 23, 18, 27, 32, 830462)),
+        ),
+    ]
--- wsgi/iportalen_django/local_static/js/iportalen.js
+++ wsgi/iportalen_django/local_static/js/iportalen.js
@@ -4043,18 +4043,27 @@ function generate_booking_form(pk){
         var bookings = data.bookings;
 
         $element.append("<div><p>" + bookable.name +"</p></div>");
-        $element.append("<div class=\"days\"></div>");
-        var $days = jQuery(".days");
+        $element.append("<div id=\"days\"></div>");
+        var $days = jQuery("#days");
         //Each day
         jQuery.each( bookings, function(index, value){
             var s = "<div class=\"single_day\">";
             var d = value.date;
-            s = s + "<p>Datum: " + d.year + "-" + d.month + "-" + d.day;
+            var month = d.month;
+            if (month <10){
+                month = "0"+month;
+            }
+            var day = d.day;
+            if (day <10){
+                day = "0"+day;
+            }
+            s = s + "<p>" + d.year + "-" + month + "-" + day;
             //Each slot:
             jQuery.each( value.slots, function( index, value2 ){
-                s = s + "<div class=\"slot";
+                s = s + "<div data-start=\""+d.year + "" + month + "" + day+"_"+value2.start_time + "\" " +
+                     "data-end=\""+d.year + "" + month + "" + day+"_"+value2.end_time + "\" class=\"slot";
                 if(value2.available){
-                    s = s + " available\">";
+                    s = s + " available\" onclick=\"select_booking_slot(this)\">";
                 } else {
                     s = s + " unavailable\">";
                 }
@@ -4063,11 +4072,77 @@ function generate_booking_form(pk){
                 s = s + "</div>";
             });
              // Write values to child!
-            console.log(s);
+
             $days.append(s);
         });
-        console.log("hit.");
     });
+    $("#id_end").attr("hidden","");
+    $("#id_start").attr("hidden","");
+}
+
+function select_booking_slot(element){
+    var $el = $(element);
+    var $container = jQuery("#days");
+    var first_click = null;
+    var last_click = null;
+    $container.find(".slot").each(function(i) {
+        var $tmp = $($container.find(".slot")[i]);
+        if ($tmp.data('clicked') == true){
+            if (first_click == null){
+                first_click = i;
+            } else {
+                last_click = i;
+            }
+        }
+    });
+    var j = $container.find(".slot").index($el);
+    if (first_click==null && last_click==null){
+        $el.data('clicked', true);
+        $el.addClass("choosen");
+    }
+    else if (first_click!=null && last_click==null){
+        if (j==first_click+1 || j==first_click-1){
+            $el.data('clicked', true);
+            $el.addClass("choosen");
+        } else if (j==first_click) {
+            $el.data('clicked', false);
+            $el.removeClass("choosen");
+        } else {
+            console.log("måste sitta ihop.");
+        }
+    }
+    else {
+        if (j==first_click-1){
+            $el.data('clicked', true);
+            $el.addClass("choosen");
+        } else if (j==last_click+1){
+            $el.data('clicked', true);
+            $el.addClass("choosen");
+        } else if (j==first_click || j==last_click) {
+            $el.data('clicked', false);
+            $el.removeClass("choosen");
+        } else {
+            console.log("måste sitta ihop.");
+        }
+    }
+    first_click = null;
+    $container.find(".slot").each(function(i) {
+        var $tmp = $($container.find(".slot")[i]);
+        if ($tmp.data('clicked') == true){
+            if (first_click == null){
+                first_click = i;
+            } else {
+                last_click = i;
+            }
+        }
+    });
+    $("#id_start").val($($container.find(".slot")[first_click]).data('start'));
+    if (last_click==null){
+        $("#id_end").val($($container.find(".slot")[first_click]).data('end'));
+    } else {
+        $("#id_end").val($($container.find(".slot")[last_click]).data('end'));
+    }
+
 };/**
  * Created by isac on 2015-10-08.
  */
