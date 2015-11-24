e835348c86dd110e2f5127837d0d14393e3db4b2
jonathan <jonan099@student.liu.se>
2015-11-24 21:39:05 +0100

    added support for max nr of slots and max nr of bookings in js.

--- wsgi/iportalen_django/grunt/js/src/booking.js
+++ wsgi/iportalen_django/grunt/js/src/booking.js
@@ -5,12 +5,10 @@ function generate_booking_form(pk, weeks_forward){
     console.log(weeks_forward);
     jQuery.get('booking/book/'+pk+"/api/"+weeks_forward+"/" , function( data ){
         var $element = jQuery(".booking");
-        var bookable = data.bookable;
+        $element.data("max_number_of_slots_in_booking", data.bookable.max_number_of_slots_in_booking);
         var bookings = data.bookings;
-
-        $element.append("<div><p>" + bookable.name +"</p></div>");
-        $element.append("<div id=\"year\"></div>");
-        $element.append("<div id=\"days\"></div>");
+        var bookable = data.bookable;
+        var user = data.user;
         var $days = jQuery("#days");
         //Each day
         var year = null;
@@ -43,7 +41,11 @@ function generate_booking_form(pk, weeks_forward){
                     if(value2.blocked){
                         s = s + " blocked\">";
                     } else {
-                        s = s + " available\" onclick=\"select_booking_slot(this)\">";
+                        s = s + " available\" ";
+                        if (user.nr_of_active_bookings<bookable.max_number_of_bookings){
+                            s = s + "onclick=\"select_booking_slot(this)\"";
+                        }
+                            s = s + ">";
                     }
                 } else {
                     s = s + " unavailable\">";
@@ -58,53 +60,62 @@ function generate_booking_form(pk, weeks_forward){
 
             $days.append(s);
         });
-        $('#year').append("<p>"+year_str+"</p>");
+        $('#year').append("<h2>"+year_str+"</h2>");
     });
     $("#id_end").attr("hidden","");
     $("#id_start").attr("hidden","");
 }
 
 function select_booking_slot(element){
+    var max_number_of_slots_in_booking = jQuery(".booking").data("max_number_of_slots_in_booking");
     var $el = $(element);
     var $container = jQuery("#days");
     var first_click = null;
     var last_click = null;
+    var it = 0;
     $container.find(".slot").each(function(i) {
         var $tmp = $($container.find(".slot")[i]);
         if ($tmp.data('clicked') == true){
             if (first_click == null){
                 first_click = i;
+                it++;
             } else {
                 last_click = i;
+                it++;
             }
         }
     });
+
     var j = $container.find(".slot").index($el);
     if (first_click==null && last_click==null){
         $el.data('clicked', true);
         $el.addClass("choosen");
     }
     else if (first_click!=null && last_click==null){
-        if (j==first_click+1 || j==first_click-1){
+        if (max_number_of_slots_in_booking!=1 && (j==first_click+1 || j==first_click-1)){
             $el.data('clicked', true);
             $el.addClass("choosen");
         } else if (j==first_click) {
             $el.data('clicked', false);
             $el.removeClass("choosen");
+        } else if(max_number_of_slots_in_booking!=1){
+            console.log("max nr of slots");
         } else {
             console.log("måste sitta ihop.");
         }
     }
     else {
-        if (j==first_click-1){
+        if (j==first_click-1 && it<max_number_of_slots_in_booking){
             $el.data('clicked', true);
             $el.addClass("choosen");
-        } else if (j==last_click+1){
+        } else if (j==last_click+1 && it<max_number_of_slots_in_booking){
             $el.data('clicked', true);
             $el.addClass("choosen");
         } else if (j==first_click || j==last_click) {
             $el.data('clicked', false);
             $el.removeClass("choosen");
+        } else if(it>=max_number_of_slots_in_booking){
+             console.log("max nr of slots");
         } else {
             console.log("måste sitta ihop.");
         }
