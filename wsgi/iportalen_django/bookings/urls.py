f009b9d43556f0d90e6d2d3f76e9f9401898185c
jonathan <jonathan@jonathananderson.se>
2015-11-26 14:22:58 +0100

    small fixing

--- wsgi/iportalen_django/bookings/urls.py
+++ wsgi/iportalen_django/bookings/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'book/(?P<bookable_id>[0-9]+)/api/$', views.api_view, name="api booking"),
     url(r'book/(?P<bookable_id>[0-9]+)/api/(?P<weeks_forward>[0-9]+)/$', views.api_view, name="api booking week_forward"),
     url(r'^invoice/(?P<invoice_id>[0-9]+)', views.invoice_pdf, name="invoice_pdf"),
-    url(r'^$', views.index, name="index"),
+    url(r'^(?P<bookable_id>[0-9]+)/delete$', views.remove_booking, name="remove_booking"),
+    url(r'^$', views.index, name="my_bookings"),
 ]
