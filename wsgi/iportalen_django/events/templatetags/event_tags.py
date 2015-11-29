72b37aecfdc4517aeb0e1605ec4b6196a557cfc4
jonathan <jonan099@student.liu.se>
2015-11-29 11:40:51 +0100

    restructured the article and event template to only have to change in one place.

--- wsgi/iportalen_django/events/templatetags/event_tags.py
+++ wsgi/iportalen_django/events/templatetags/event_tags.py
@@ -16,9 +16,15 @@ def get_all_events():
     return events
 
 @register.assignment_tag
-def get_event(pk):
-    event = Event.objects.get(pk=pk)
-    return event
+def get_event(event, user):
+    can_administer = event.can_administer(user)
+    if event.status == Event.APPROVED or can_administer:
+        return {
+            "can_administer": can_administer,
+            "registered": event.registered(user),
+        }
+    else:
+        return None
 
 @register.assignment_tag
 def get_organisation_events(organisation_pk):
