c8ee259b4146a545c9b49dd7e490aaf2621d2fe7
jonathan <jonathan@jonathananderson.se>
2015-12-14 01:06:47 +0100

    more changes to evaluate course app

--- wsgi/iportalen_django/course_evaluations/urls.py
+++ wsgi/iportalen_django/course_evaluations/urls.py
@@ -9,7 +9,9 @@ urlpatterns = [
     url(r'^admin/year/(?P<year>[0-9]+)/$', view=views.admin_year, name="admin year"),
     url(r'^admin/period/(?P<pk>[0-9]+)/$', view=views.admin_period, name="admin period"),
     url(r'^admin/period/(?P<pk>[0-9]+)/edit$', view=views.edit_period, name="edit period"),
+    url(r'^admin/period/(?P<pk>[0-9]+)/evaluations/$', view=views.evaluations, name="evaluations"),
     url(r'^admin/courses/$', view=views.create_courses, name="create courses"),
+    url(r'^admin/rewards/$', view=views.create_or_modify_rewards, name="edit rewards"),
     #url(r'^admin/courses/edit$', view=views.create_or_modify_courses, name="edit course"),
 
     ]
