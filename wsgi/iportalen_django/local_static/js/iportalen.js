14790827e237a8ee441dd61f45c8988f47ad804d
jonathan <jonan099@student.liu.se>
2015-12-18 22:12:02 +0100

    added markdown editor for organisation

--- wsgi/iportalen_django/local_static/js/iportalen.js
+++ wsgi/iportalen_django/local_static/js/iportalen.js
@@ -4511,6 +4511,30 @@ var event_preview = function () {
 
 
 ;/**
+ * Created by jonathan on 2015-12-18.
+ */
+//This function initiates the markdown engine. It is called on by event_preview below.
+function markdown_organisation_preview() {
+        var converter = Markdown.getSanitizingConverter();
+        converter.hooks.chain("preConversion", function (text) {
+            return text.replace(/[&<"'\/]/g, function (s) {
+                var entityMap = {
+                    "&": "&amp;",
+                    "<": "&lt;",
+                    '"': '&quot;',
+                    "'": '&#39;',
+                    "/": '&#x2F;'
+                };
+                return entityMap[s];
+            }).replace(/([#]{2,})/g, '#').replace(/([=]{3,})/g, '').replace(/([-]{3,})/g, '').replace(/([`])/g, '');
+        });
+        converter.hooks.chain("plainLinkText", function (url) {
+            return url.replace(/^https?:\/\//, "");
+        });
+        var editor = new Markdown.Editor(converter, "-body");
+        editor.run();
+    }
+;/**
  * Created by jonathan on 2015-10-20.
  */
     var shuffle_sponsors = function () {
