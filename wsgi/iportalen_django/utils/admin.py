2c35a4324a79eaee68bbbbfb53fe426ff25b8aeb
jonathan <jonan099@student.liu.se>
2016-01-04 12:59:07 +0100

    added a adminmodel to hide some models from the admin page to prevent faulty input

---
+++ wsgi/iportalen_django/utils/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+
+
+class HiddenModelAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        if request.user.is_superuser:
+            return True
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        if request.user.is_superuser:
+            return True
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        if request.user.is_superuser:
+            return True
+        return False
