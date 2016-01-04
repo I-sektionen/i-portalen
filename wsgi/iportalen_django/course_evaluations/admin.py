cb35309ffebc82d818eefac019c2ee7d7a09ee81
jonathan <jonan099@student.liu.se>
2016-01-04 12:59:07 +0100

    added a adminmodel to hide some models from the admin page to prevent faulty input

--- wsgi/iportalen_django/course_evaluations/admin.py
+++ wsgi/iportalen_django/course_evaluations/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from .models import Course, Reward, Evaluation, Period, Year, CourseEvaluationSettings
+from utils.admin import HiddenModelAdmin
 
-admin.site.register(Year)
-admin.site.register(Course)
-admin.site.register(Reward)
-admin.site.register(Evaluation)
-admin.site.register(Period)
+admin.site.register(Year, HiddenModelAdmin)
+admin.site.register(Course, HiddenModelAdmin)
+admin.site.register(Reward, HiddenModelAdmin)
+admin.site.register(Evaluation, HiddenModelAdmin)
+admin.site.register(Period, HiddenModelAdmin)
 admin.site.register(CourseEvaluationSettings)
--- wsgi/iportalen_django/tags/admin.py
+++ wsgi/iportalen_django/tags/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-
+from utils.admin import HiddenModelAdmin
 from .models import Tag
 
 # Register your models here.
-admin.site.register(Tag)
+admin.site.register(Tag, HiddenModelAdmin)
