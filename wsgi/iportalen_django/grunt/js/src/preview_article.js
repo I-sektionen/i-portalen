77d4911f31c6c9ea63f1f1c93cef55dbbf6f0b56
Isac Ekberg <isaek808@student.liu.se>
2016-01-16 14:34:16 +0100

    Articles now being previewed correct.

--- wsgi/iportalen_django/grunt/js/src/preview_article.js
+++ wsgi/iportalen_django/grunt/js/src/preview_article.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
 });
 
 
-function markdown_event_preview() {
+function markdown_article_preview() {
         var converter = Markdown.getSanitizingConverter();
         converter.hooks.chain("preConversion", function (text) {
             return text.replace(/[&<"'\/]/g, function (s) {
@@ -45,17 +45,17 @@ function markdown_event_preview() {
  */
 var article_preview = function () {
     //Start the engines. Listens to the body and outputs.
-    console.log("Call on meeeee");
-    markdown_event_preview();
-    console.log("Call on me");
+    markdown_article_preview();
 
     //These are the elements that is being listened to.
     var listened_to = [];
     var headline = $("#id_headline");
     var lead = $("#id_lead");
-    var author = $("#id_author");
+    var organisation = $("#id_organisations");
     var from = $("#id_visible_from");
     var tags = $("#id_tags");
+    var sponsor = $("#id_sponsored");
+    //TODO: Ladda ned fil
 
     //This is the elements where the results are printed to.
     var headline_preview = $(".headline_preview");
@@ -63,17 +63,18 @@ var article_preview = function () {
     var author_preview = $(".author_preview");
     var from_preview_date = $(".date_preview");
     var tags_preview = $(".tags_preview");
+    var sponsor_preview = $(".sponsor_preview");
 
     listened_to.push(
                     [headline, headline_preview],
                     [lead, lead_preview],
-                    [author, author_preview],
+                    [organisation, author_preview],
                     [from, from_preview_date],
-                    [tags, tags_preview]
+                    [tags, tags_preview],
+                    [sponsor, sponsor_preview]
     );
 
     var render = function (){
-        console.log("Bamm.");
         jQuery.each(listened_to, function(index, element){
             var txt;
 
@@ -84,18 +85,36 @@ var article_preview = function () {
                     var date = d[0];
                     element[1].text(date);
                 }
-            } else if(element[0].is(tags)){
+            } else if(element[0].is(tags)) {
                 var selected_tags = tags.children().filter(":selected");
                 var tag_string = "";
-                jQuery.each(selected_tags, function(index, ele){
+                jQuery.each(selected_tags, function (index, ele) {
                     tag_string = tag_string + ele.innerHTML + ", ";
                 });
-                if(tag_string != ""){
-                    tag_string = tag_string.substring(0, tag_string.length-2)
+                if (tag_string != "") {
+                    tag_string = tag_string.substring(0, tag_string.length - 2)
                 }
                 element[1].text(tag_string);
-
-            } else {
+            } else if(element[0].is(sponsor)) {
+                if(sponsor.prop('checked')){
+                    sponsor_preview.text("Sponsrat innehåll");
+                } else {
+                    sponsor_preview.text("");
+                }
+            } else if(element[0].is(organisation)) {
+                var selected_org = organisation.children().filter(":selected");
+                var org_string = "";
+                jQuery.each(selected_org, function(index, ele){
+                    org_string = ele.innerHTML;
+                });
+                if(org_string == ""){
+                    author_preview.text("Ditt namn");
+                } else {
+                    author_preview.text(org_string);
+                }
+            }
+            else
+            {
                 txt = element[0].val();
                 element[1].text(txt);
             }
