c039aec9b6ac21a0e20b8c0aaed79dec8acbc1c3
Isac Ekberg <isaek808@student.liu.se>
2016-01-21 14:13:15 +0100

    Added organsiatons to sitemap

--- wsgi/iportalen_django/iportalen/urls.py
+++ wsgi/iportalen_django/iportalen/urls.py
@@ -16,12 +16,13 @@ from events import urls as event_urls
 from organisations import urls as org_urls
 from bookings import urls as booking_urls
 from course_evaluations import urls as course_urls
-from .sitemaps import StaticViewSitemapHigh, EventSitemap, ArticleSitemap, StaticViewSitemapLow
+from .sitemaps import StaticViewSitemapHigh, EventSitemap, ArticleSitemap, StaticViewSitemapLow, OrganisationSitemap
 
 sitemaps = {
     'static': StaticViewSitemapHigh,
     'event': EventSitemap,
     'article': ArticleSitemap,
+    'organisations': OrganisationSitemap,
     'static_low': StaticViewSitemapLow
 }
 
