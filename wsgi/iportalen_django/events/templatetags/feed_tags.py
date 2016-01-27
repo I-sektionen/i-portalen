6aa90e2f28da20700b88ec9f7669839691c27f91
Isac Ekberg <isaek808@student.liu.se>
2016-01-27 21:53:54 +0100

    Menu item added and removed unused tag.

--- wsgi/iportalen_django/events/templatetags/feed_tags.py
+++ wsgi/iportalen_django/events/templatetags/feed_tags.py
@@ -1,17 +1,13 @@
 from django.template.loader_tags import register
-from events.models import Event
-
-
-@register.assignment_tag()
-def all_calendar_events():
-    events = Event.objects.published()
-    return events
 
 
 @register.simple_tag()
 def print_event(event):
-    t_format = "%Y%m%dT%H%M%S"
-
+    def _descr(e):
+        s1 = e.lead
+        s2 = " Eventet finns på: www.i-portalen.se" + e.get_absolute_url()
+        return s1 + s2
+    t_format = "%Y%m%dT%H%M%S"  # Formating style used by the ics-file format.
 
     s = "BEGIN:VEVENT\n\
 DTSTART:{start}\n\
@@ -21,11 +17,11 @@ LOCATION:{location}\n\
 DESCRIPTION:{desc}\n\
 UID: {uid}\n\
 URL:{url}\n\
-END:VEVENT\n".format(start=event.start.strftime(t_format),
+END:VEVENT\n".format(start=event.start.strftime(t_format),  # TODO: Add timezone information.
                      end=event.end.strftime(t_format),
                      summary=event.headline,
                      location=event.location,
-                     desc=event.lead,
+                     desc=_descr(event),
                      url="www.development.i-portalen.se" + event.get_absolute_url(),
                      uid="IPORTALEN_DJANGO_1337" + str(event.pk))  # Unique identifier
     return s
