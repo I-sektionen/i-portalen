3ad98d159fbdefdbea464339c2e709aa84d07522
jonathan <jonathan@jonathananderson.se>
2016-01-30 21:17:09 +0100

    bugfix on personal feed

--- wsgi/iportalen_django/events/templatetags/feed_tags.py
+++ wsgi/iportalen_django/events/templatetags/feed_tags.py
@@ -9,19 +9,20 @@ def print_event(event):
         return s1 + s2
     t_format = "%Y%m%dT%H%M%S"  # Formating style used by the ics-file format.
 
-    s = "BEGIN:VEVENT\n\
-DTSTART:{start}\n\
-DTEND:{end}\n\
-SUMMARY:{summary}\n\
-LOCATION:{location}\n\
-DESCRIPTION:{desc}\n\
-UID: {uid}\n\
-URL:{url}\n\
-END:VEVENT\n".format(start=event.start.strftime(t_format),  # TODO: Add timezone information.
-                     end=event.end.strftime(t_format),
-                     summary=event.headline,
-                     location=event.location,
-                     desc=_descr(event),
-                     url="www.development.i-portalen.se" + event.get_absolute_url(),
-                     uid="IPORTALEN_DJANGO_1337" + str(event.pk))  # Unique identifier
+    s = "".join(["BEGIN:VEVENT\n",
+                 "DTSTART:{start}\n",
+                 "DTEND:{end}\n",
+                 "SUMMARY:{summary}\n",
+                 "LOCATION:{location}\n",
+                 "DESCRIPTION:{desc}\n",
+                 "UID: {uid}\n",
+                 "URL:{url}\n",
+                 "END:VEVENT\n"]).format(
+        start=event.start.strftime(t_format),  # TODO: Add timezone information.
+        end=event.end.strftime(t_format),
+        summary=event.headline,
+        location=event.location,
+        desc=_descr(event),
+        url="www.development.i-portalen.se" + event.get_absolute_url(),
+        uid="IPORTALEN_DJANGO_1337" + str(event.pk))  # Unique identifier
     return s
