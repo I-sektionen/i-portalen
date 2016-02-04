f5d615e438b426cc48e6e03e5a697a5c77a8f6f7
jonathan <jonathan@jonathananderson.se>
2016-02-05 00:43:14 +0100

    More work on voting models, migration, and urls

--- wsgi/iportalen_django/votings/models.py
+++ wsgi/iportalen_django/votings/models.py
@@ -1,9 +1,10 @@
+from django.core.urlresolvers import reverse
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
-
+from django.utils.translation import ugettext_lazy as _, ugettext
 from events.models import Event
 from iportalen import settings
 from utils import time
+from .exceptions import CouldNotVoteException
 
 
 class QuestionGroup(models.Model):
@@ -33,6 +34,14 @@ class QuestionGroup(models.Model):
     def __str__(self):
         return "id: {pk}".format(pk=self.pk)
 
+    def get_absolute_url(self):
+        """Get url of object"""
+        return reverse('votings:question group', kwargs={'pk': self.pk})
+
+    class Meta:
+        verbose_name = _("frågegrupp")
+        verbose_name_plural = _("frågegrupper")
+
 
 class Question(models.Model):
     OPEN = 'o'
@@ -49,7 +58,7 @@ class Question(models.Model):
         (PUBLIC_LIMITED, _("Publik tillgång till begränsad information om röstningen.")),
         (PRIVATE, _("Privat åtkomst enbart för administratörer")),
     )
-    question_group = models.ForeignKey(QuestionGroup, verbose_name=_('namn'),)
+    question_group = models.ForeignKey(QuestionGroup, verbose_name=_('frågegrupp'),)
     name = models.CharField(verbose_name=_('namn'), max_length=255)
     body = models.TextField(verbose_name=_("utförlig information"), help_text=_("Utförligare information till frågan."))
     result = models.CharField(
@@ -64,7 +73,8 @@ class Question(models.Model):
         default=CLOSED,
         blank=False,
         null=False)
-    nr_of_picks = models.IntegerField(verbose_name=_("Antal val en användare kan kryssa i på frågan."))
+    nr_of_picks = models.IntegerField(verbose_name=_("Antal val en användare kan kryssa i på frågan."), default=1)
+    anonymous = models.BooleanField(verbose_name=_('namn'), default=True)
     modified_by = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         verbose_name=_("användare"),
@@ -75,13 +85,49 @@ class Question(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        """Get url of object"""
+        return reverse('votings:question', kwargs={'pk': self.pk})
+
     class Meta:
         verbose_name = _("fråga")
         verbose_name_plural = _("frågor")
 
 
 class Options(models.Model):
-    pass
+    name = models.CharField(verbose_name=_("alternativ"), max_length=255)
+    question = models.ForeignKey(Question, verbose_name=_("fråga"))
+
+    def __str__(self):
+        return "{question}, {name}".format(name=self.name, question=self.question.name)
+
+    class Meta:
+        verbose_name = _("alternativ")
+        verbose_name_plural = _("alternativen")
+
 
 class Votes(models.Model):
-    pass
+    question = models.ForeignKey(Question, verbose_name=_("fråga"))
+    option = models.ForeignKey(Options, verbose_name=_("alternativ"))
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_("användare"), null=True, blank=True)
+
+    class Meta:
+        verbose_name = _("röst")
+        verbose_name_plural = _("röster")
+
+    def save(self, *args, **kwargs):
+        if not self.question.hasvoted_set.filter(user=self.user).exists():
+            if self.question.anonymous and self.user:
+                self.user = None
+            super(Votes, self).save(*args, **kwargs)
+        else:
+            raise CouldNotVoteException(reason=ugettext("User has already voted."))
+
+
+class HasVoted(models.Model):
+    question = models.ForeignKey(Question, verbose_name=_("fråga"))
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_("användare"))
+
+    class Meta:
+        verbose_name = _("deltagare i omröstningen")
+        verbose_name_plural = _("deltagarna i omröstningen")
--- wsgi/iportalen_django/votings/urls.py
+++ wsgi/iportalen_django/votings/urls.py
@@ -1,6 +1,13 @@
-try:
-    from django.conf.urls import *
-except ImportError:  # django < 1.4
-    from django.conf.urls.defaults import *
+from django.conf.urls import url, include
+from . import views
 
-# place app url patterns here
+app_name = "votings"
+
+voting_patterns = [
+    url(r'^$',                                           view=views.votings,          name='votings'),
+    url(r'^(?P<qg_pk>[0-9]+)/$',                         view=views.question_group,   name='question group'),
+    url(r'^(?P<qg_pk>[0-9]+)/(?P<q_pk>[0-9]+)/$',        view=views.question_details, name='question'),
+    url(r'^(?P<qg_pk>[0-9]+)/(?P<q_pk>[0-9]+)/result/$', view=views.question_result,  name='result'),
+]
+
+urlpatterns = [url(r'^', include(voting_patterns, namespace=app_name))]
