fe3798f5a73c0b7309e0a542898d3f03e003615f
jonathan <jonathan@jonathananderson.se>
2016-02-05 00:43:14 +0100

    More work on voting models, migration, and urls

--- wsgi/iportalen_django/votings/views.py
+++ wsgi/iportalen_django/votings/views.py
@@ -1,1 +1,15 @@
 # Create your views here.
+def votings(request):
+    return None
+
+
+def question_group(request):
+    return None
+
+
+def question_details(request):
+    return None
+
+
+def question_result(request):
+    return None
