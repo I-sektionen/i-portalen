1e0e6883c8f03ecdc380db6833af1d666a467dd8
Isac Ekberg <isaek808@student.liu.se>
2016-02-06 18:29:41 +0100

    Removed runserver-grunt itegration

--- wsgi/iportalen_django/iportalen/apps.py
+++ wsgi/iportalen_django/iportalen/apps.py
@@ -1,13 +1,6 @@
 from django.apps import AppConfig
-from django.conf import settings
-from utils.grunt import GruntProcess
 
 
 class IportalenAppConfig(AppConfig):
     name = 'iportalen'
     verbose_name = 'I-Portalen'
-
-    def ready(self):
-        if settings.RUN_GRUNT:
-            g = GruntProcess()
-            g.start_grunt()
--- wsgi/iportalen_django/utils/grunt.py
+++ wsgi/iportalen_django/utils/grunt.py
@@ -4,6 +4,11 @@ import os
 import signal
 import atexit
 
+# This class is trying to create a subprocess running grunt.
+# The goal was to make it idempotent and run once.
+# Newer got it to run properly, so it is disabled.
+#  / Isac
+
 
 class GruntProcess:
     def __init__(self):
@@ -15,8 +20,8 @@ class GruntProcess:
             return
         print('>>> Starting grunt! :)')
         self.grunt_process = subprocess.Popen(
-            ['npm install {0}'.format(os.path.join(settings.BASE_DIR, 'grunt')),
-             'grunt --gruntfile={0}/Gruntfile.js --base=.'.format(os.path.join(settings.BASE_DIR, 'grunt'))],
+            ['npm install {0}'.format(settings.BASE_DIR),
+             'grunt --gruntfile={0}/Gruntfile.js --base=.'.format(settings.BASE_DIR)],
             shell=True,
         )
         print('>>> Gruntprocess on pid {0}'.format(self.grunt_process.pid))
