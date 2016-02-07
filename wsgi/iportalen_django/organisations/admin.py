4b41912967fa955b412c81909e89173fd24807da
jonathan <jonathan@jonathananderson.se>
2016-02-07 21:24:12 +0100

    added redirect links for old urls

--- wsgi/iportalen_django/organisations/admin.py
+++ wsgi/iportalen_django/organisations/admin.py
@@ -1,11 +1,13 @@
 from .models import Organisation, OrganisationPost
 from utils.admin import HiddenModelAdmin, iportalen_admin_site
 
+
 class OrganisationsAdmin(HiddenModelAdmin):
-    readonly_fields =("modified_by",)
+    readonly_fields = ("modified_by",)
+
 
 class OrganisationsPostAdmin(HiddenModelAdmin):
-    readonly_fields =("modified_by",)
+    readonly_fields = ("modified_by",)
 
 iportalen_admin_site.register(Organisation, OrganisationsAdmin)
 iportalen_admin_site.register(OrganisationPost, OrganisationsPostAdmin)
