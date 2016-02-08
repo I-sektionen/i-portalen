1ede22ae058328f7eeb02ed02c77cfb39171ccbf
jonathan <jonathan@jonathananderson.se>
2016-02-08 11:11:19 +0100

    working on result view

--- wsgi/iportalen_django/votings/admin.py
+++ wsgi/iportalen_django/votings/admin.py
@@ -4,3 +4,7 @@ from utils.admin import HiddenModelAdmin, iportalen_admin_site
 iportalen_admin_site.register(models.QuestionGroup, HiddenModelAdmin)
 iportalen_admin_site.register(models.Question, HiddenModelAdmin)
 iportalen_admin_site.register(models.Option, HiddenModelAdmin)
+
+
+# iportalen_admin_site.register(models.Vote, HiddenModelAdmin)
+# iportalen_admin_site.register(models.HasVoted, HiddenModelAdmin)
