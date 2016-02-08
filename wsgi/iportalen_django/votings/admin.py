4ab8a3886996011f0ad0865a39deb4a51893071c
jonathan <jonathan@jonathananderson.se>
2016-02-08 12:47:50 +0100

    Added optional verification code for question.

--- wsgi/iportalen_django/votings/admin.py
+++ wsgi/iportalen_django/votings/admin.py
@@ -6,5 +6,5 @@ iportalen_admin_site.register(models.Question, HiddenModelAdmin)
 iportalen_admin_site.register(models.Option, HiddenModelAdmin)
 
 
-# iportalen_admin_site.register(models.Vote, HiddenModelAdmin)
-# iportalen_admin_site.register(models.HasVoted, HiddenModelAdmin)
+iportalen_admin_site.register(models.Vote, HiddenModelAdmin)
+iportalen_admin_site.register(models.HasVoted, HiddenModelAdmin)
