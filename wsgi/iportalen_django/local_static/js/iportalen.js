6d7e743db37b32d72180abf0450005d2462e5579
Jesper Lehtonen <jesle149@student.liu.se>
2016-02-11 18:52:24 +0100

    Added speaker list to administer speaker list

--- wsgi/iportalen_django/local_static/js/iportalen.js
+++ wsgi/iportalen_django/local_static/js/iportalen.js
@@ -507,6 +507,38 @@ function speaker_list_admin(url){
         });
         input_field.val('');
     });
+}
+
+
+function speaker_list_view_admin(url) {
+    init_csrf();
+
+    var s_list = $("#speaker_list_admin_nonformat").find("ol");
+    var t=setInterval(reload_list, 1000);
+    function reload_list() {
+
+        var data = {
+            'method': 'all'
+        };
+        $.ajax({
+            "type": "POST",
+            "dataType": "json",
+            "url": url,
+            "data": data,
+            "success": function (result) {
+                if (result.status === "ok") {
+                    var speakerlist = result.speaker_list;
+                    var arrayLength = speakerlist.length;
+                    s_list.empty();
+                    for (var i = 0; i < arrayLength; i++) {
+                        s_list.append('<li>' + speakerlist[i].first_name + ' ' + speakerlist[i].last_name + '</li>');
+                    }
+                } else {
+                    console.log(result.status);
+                }
+            }
+        });
+    }
 };/**
  * Created by jonathan on 2015-11-09.
  */
