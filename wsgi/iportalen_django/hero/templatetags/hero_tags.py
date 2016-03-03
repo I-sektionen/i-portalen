d4c3c0ff1a6d22ea475973d9fddc570f75c86893
jonathan <jonathan@jonathananderson.se>
2016-03-03 01:12:03 +0100

    added menu for hero

--- wsgi/iportalen_django/hero/templatetags/hero_tags.py
+++ wsgi/iportalen_django/hero/templatetags/hero_tags.py
@@ -1,6 +1,8 @@
+from django.core.urlresolvers import reverse
 from django.template.loader_tags import register
 from django.utils import timezone
 from hero.models import Hero
+from django.utils.translation import ugettext as _
 
 
 @register.simple_tag()
@@ -11,3 +13,12 @@ def get_hero_url():
     else:
         # Fallback
         return "https://s3.amazonaws.com/iportalen-us/static/iportalen/isektionen_logo.png"
+
+
+@register.assignment_tag
+def get_menu_choices_hero(user):
+    menu_choices = []
+    if user.has_perm("hero.change_hero"):
+        menu_choices.append((_('Ändra hero'), reverse('iportalenadmin:app_list', args=('hero',))))
+
+    return menu_choices
