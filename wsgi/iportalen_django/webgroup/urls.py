81922e778a233b0abe0d99604aac20c10165500f
jonathan <jonathan@jonathananderson.se>
2017-04-11 16:15:06 +0200

    Added a github stats page.

--- wsgi/iportalen_django/webgroup/urls.py
+++ wsgi/iportalen_django/webgroup/urls.py
@@ -5,7 +5,8 @@ app_name = 'webgroup'
 
 
 webgroup_patterns = [
-    url(r'$',                   view=views.landing,   name="landing"),
+    url(r'^$',                   view=views.landing,   name="landing"),
+    url(r'^github/$',                   view=views.github_stats,   name="github"),
 ]
 
 urlpatterns = [url(r'^', include(webgroup_patterns, namespace=app_name))]
--- wsgi/iportalen_django/webgroup/views.py
+++ wsgi/iportalen_django/webgroup/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def landing(request):
     return render(request, "webgroup/landing.html", {})
+
+
+def github_stats(request):
+    return render(request, "webgroup/github.html", {})
