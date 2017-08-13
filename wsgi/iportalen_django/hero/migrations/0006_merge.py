f5b015abf3d29dbaeb69c8225009126e61784aee
Anton Revin <anton.f.revin@gmail.com>
2017-08-13 14:46:35 +0200

    Old migration fix

---
+++ wsgi/iportalen_django/hero/migrations/0006_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('hero', '0005_auto_20160531_1436'),
+        ('hero', '0005_auto_20160306_1449'),
+    ]
+
+    operations = [
+    ]
