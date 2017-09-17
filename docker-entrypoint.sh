c1799620989de953f47dcb905d186b72edf50d71
jonathan <jonathan@jonathananderson.se>
2017-09-17 20:36:29 +0200

    Working on deployment to AWS

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-git pull origin docker
+git pull origin master
 /usr/bin/pip3 install -r ../requirements.txt
 export PYTHONPATH=$PYTHONPATH:/srv/wsgi:/srv/wsgi/iportalen_django
 /usr/bin/python3 iportalen_django/manage.py migrate        # Apply database migrations
