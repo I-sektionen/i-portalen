45a6589397f93a9f27222e6f488528db55e93c14
Jesper Hellström <jeshe316@student.liu.se>
2017-11-14 18:01:16 +0100

    631: Change the word_counter form to be readonly and also added a x/160 counter.

--- wsgi/iportalen_django/articles/js/src/word_counter.js
+++ wsgi/iportalen_django/articles/js/src/word_counter.js
@@ -6,7 +6,7 @@
 $("#id_lead").on("change paste keyup keypress", function(){
     console.log("funktion");
     var cs = $("#id_lead").val().length;
-    document.getElementById("characters").value = cs;
+    document.getElementById("characters").value = cs+"/160";
 });
 
 
