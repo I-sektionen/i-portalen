294f1faf07fceaa8b8ad301bad14b9cc366e0bfd
Jesper Hellström <jeshe316@student.liu.se>
2017-11-14 18:37:58 +0100

    631: Change the article_lead to only allow 160 characters in the input form.

--- wsgi/iportalen_django/articles/js/src/word_counter.js
+++ wsgi/iportalen_django/articles/js/src/word_counter.js
@@ -3,11 +3,18 @@
 
 //$('#characters').text(cs);
 // }
+
+$(document).ready(function()
+{
+    $("#id_lead").attr('maxlength','160');
+});
+
 $("#id_lead").on("change paste keyup keydown keypress", function(){
     console.log("funktion");
     var cs = $("#id_lead").val().length;
     document.getElementById("characters").value = cs+"/160";
 });
+
 
 
 //$('#id_lead').on('input', updateCount());
