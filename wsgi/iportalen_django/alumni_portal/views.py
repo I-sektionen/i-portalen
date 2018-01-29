6d4d371b2b02a8d6d9d6b01eb85874256670c9cf
Anton Revin <anton.f.revin@gmail.com>
2018-01-29 18:38:11 +0100

    Hot fix

--- wsgi/iportalen_django/alumni_portal/views.py
+++ wsgi/iportalen_django/alumni_portal/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 def alumni_portal(request):
     print("!!!!!")
-    return render(request, 'alumni_portal/alumni_portal.html')
+    return render(request, 'alumni_portal.html')
 
