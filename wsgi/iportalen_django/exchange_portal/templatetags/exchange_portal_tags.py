ffa28a172da0299eb9219e9daa6938ac4c0fbc4d
erikkarvonen <erik.karvonen95@gmail.com>
2018-02-21 18:36:28 +0100

    Updated admin pages + views

---
+++ wsgi/iportalen_django/exchange_portal/templatetags/exchange_portal_tags.py
@@ -0,0 +1,11 @@
+#@register.assignment_tag
+#def get_menu_organisations():
+ #   organisations = {
+  #      "sektionen": list(Organisation.objects.filter(
+   #         organisation_type=Organisation.SEKTIONEN,
+    #    ).order_by('name')),
+     #   "foreningar": list(Organisation.objects.filter(
+      #      organisation_type=Organisation.FORENINGAR,
+       # ).order_by('name')),
+ #   }
+#    return organisations
